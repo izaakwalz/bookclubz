fdbb1f40c62c19ddfc2f8eb46196af0e7c4332e0
izaakwalz <iblack.xyz@gmail.com>
2021-05-05 02:43:24 +0100

    removed unused variables

--- src/views/Home/components/BookClub/BookClub.js
+++ src/views/Home/components/BookClub/BookClub.js
@@ -2,17 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import {
-  useMediaQuery,
-  Grid,
-  Typography,
-  Button,
-  Divider,
-} from '@material-ui/core';
+import { useMediaQuery, Grid, Typography, Button } from '@material-ui/core';
 import { Image, LearnMoreLink } from '../../../../components/atoms';
 import { SectionHeader } from '../../../../components/molecules';
 import { PeopleOutlineSharp } from '@material-ui/icons';
-import { Section } from '../../../../components/organisms';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
